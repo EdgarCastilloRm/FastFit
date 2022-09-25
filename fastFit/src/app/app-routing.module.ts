430be59bba185ba16ef5b97f7322e14353a13407
Edgar Castillo Ramírez <A00827826@itesm.mx>
2022-09-24 20:46:28 -0500

    Commit Login
    Full login implementation (backend included)

--- fastFit/src/app/app-routing.module.ts
+++ fastFit/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InnerGuard } from './guards/inner.guard';
+import { LoginGuard } from './guards/login.guard';
 import { AccountComponent } from './views/account/account/account.component';
 import { GarmentsComponent } from './views/garments/garments/garments.component';
 import { GenerateComponent } from './views/generate/generate/generate.component';
@@ -8,12 +10,12 @@ import { LikedComponent } from './views/liked/liked/liked.component';
 import { LoginMainComponent } from './views/login/login-main/login-main.component';
 
 const routes: Routes = [
-  {path: '', component: LoginMainComponent},
-  {path: 'home', component: HomeComponent},
-  {path: 'liked', component: LikedComponent},
-  {path: 'generate', component: GenerateComponent},
-  {path: 'garments', component: GarmentsComponent},
-  {path: 'account', component: AccountComponent}
+  {path: '', component: LoginMainComponent, canActivate: [LoginGuard]},
+  {path: 'home', component: HomeComponent, canActivate: [InnerGuard]},
+  {path: 'liked', component: LikedComponent, canActivate: [InnerGuard]},
+  {path: 'generate', component: GenerateComponent, canActivate: [InnerGuard]},
+  {path: 'garments', component: GarmentsComponent, canActivate: [InnerGuard]},
+  {path: 'account', component: AccountComponent, canActivate: [InnerGuard]}
 ];
 
 @NgModule({
--- fastFit/src/app/views/login/login.module.ts
+++ fastFit/src/app/views/login/login.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginMainComponent } from './login-main/login-main.component';
 import { AngularMaterialsModule } from 'src/app/shared/angular-materials/angular-materials.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MicrosoftLoginProvider, SocialAuthServiceConfig, SocialLoginModule } from '@abacritt/angularx-social-login';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -15,7 +17,30 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     AngularMaterialsModule,
     FormsModule,
     ReactiveFormsModule,
-    FlexLayoutModule
-  ]
+    FlexLayoutModule,
+    SocialLoginModule,
+    HttpClientModule
+  ],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: true,
+        providers: [
+          {
+            id: MicrosoftLoginProvider.PROVIDER_ID,
+            provider: new MicrosoftLoginProvider('39f60a81-6664-4d23-95d4-9291205373af', {
+              redirect_uri: 'http://localhost:4200',
+              logout_redirect_uri: 'http://localhost:4200/'
+            }),
+          }
+        ],
+        onError: (err) => {
+          console.error(err);
+        }
+      } as SocialAuthServiceConfig,
+    }
+  ],
+  schemas : [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class LoginModule { }
